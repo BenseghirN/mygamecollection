2bcce0312e66186728bd3da5f32f2546839fa02e
BENSEGHIR Noris <n0r1s@live.be>
2026-04-24 09:13:27 +0200

    make db session connexion auto dispose

--- src/mygamecollection/infrastructure/database/session.py
+++ src/mygamecollection/infrastructure/database/session.py
@@ -1,9 +1,21 @@
+from contextlib import asynccontextmanager
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from src.mygamecollection.infrastructure.database.sqlalchemy_db_context import SQLAlchemyGameCollectionDbContext
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL environment variable not found")
 
 engine = create_async_engine(DATABASE_URL)
-SessionLocal = async_sessionmaker(engine, autocommit=False, autoflush=False, expire_on_commit=False)
+SessionLocal = async_sessionmaker(
+    engine, 
+    autocommit=False, 
+    autoflush=False, 
+    expire_on_commit=False
+)
+
+@asynccontextmanager
+async def get_db_session():
+    async with SessionLocal() as session:
+        yield SQLAlchemyGameCollectionDbContext(session)
